0fab22ac9d2b8c56da7e8bbfd1aad51682eb6d68
Shraddha <118170804+shradiphylleia@users.noreply.github.com>
2024-07-29 10:32:12 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,8 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Auth0Provider
-    domain="dev-yc4z8u55kz4uyg53.us.auth0.com" 
-    clientId="bYI9PaX7q6vSTVa0OGghzkksfBrrDbsh"
+    domain="" //key here
+    clientId="" //key here
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
