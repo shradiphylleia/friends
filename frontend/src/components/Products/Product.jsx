b3480d5ccce7836681aca8f1dbbe56beda30eefe
Shraddha <118170804+shradiphylleia@users.noreply.github.com>
2024-08-01 00:09:05 +0530

    Product and Service page

--- frontend/src/components/Products/Product.jsx
+++ frontend/src/components/Products/Product.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import NavLinks from "../NavLinks";
+import Footer from "../Footer"
+import Box from "../Box"
+import TextExplanation from "../TextExplanation"
+
+import './product.css'
 
 function Product() {
   return (
-    <div>Product</div>
-  )
+    <div>
+      <div className="Navbar">
+        <div className="LeftNav">
+          <NavLinks className="NavLink" name="About Us" redirect="/about" />
+          <NavLinks className="NavLink" name="Services" redirect="/services" />
+          <NavLinks className="NavLink" name="Product" redirect="/product" />
+        </div>
+        <div className="Home">
+          <NavLinks className="HomeLink" name="friends" redirect="/" />
+        </div>
+      </div>
+      <div className="OfferDetails">
+      <Box className="Products" ButtonClassName='OfferButton' onButtonClick={console.log("hi")} />
+      <TextExplanation className="ServiceText"  text="Ability to book consultations,schedule vaccination reminders and book pet care provides" /> 
+      </div>
+      <div className="Footer">
+        <Footer/>
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
--- frontend/src/components/Services/Service.jsx
+++ frontend/src/components/Services/Service.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import NavLinks from "../NavLinks";
+import Footer from "../Footer"
+import Box from "../Box"
+import TextExplanation from "../TextExplanation"
 
-function Service() {
+import './service.css'
+
+function Product() {
   return (
-    <div>Service</div>
-  )
+    <div>
+      <div className="Navbar">
+        <div className="LeftNav">
+          <NavLinks className="NavLink" name="About Us" redirect="/about" />
+          <NavLinks className="NavLink" name="Services" redirect="/services" />
+          <NavLinks className="NavLink" name="Product" redirect="/product" />
+        </div>
+        <div className="Home">
+          <NavLinks className="HomeLink" name="friends" redirect="/" />
+        </div>
+      </div>
+      <div className="OfferDetails">
+      <Box className="Services" ButtonClassName='OfferButton' onButtonClick={console.log("hi")} />
+      <TextExplanation className="ServiceText"  text="Ability to book consultations,schedule vaccination reminders and book pet care provides" /> 
+      </div>
+      <div className="Footer">
+        <Footer/>
+      </div>
+    </div>
+  );
 }
 
-export default Service
+export default Product;
